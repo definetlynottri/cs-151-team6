1d7371dc2c307e40fc3f6cb5bdb49c2b59dbcb52
Tri Tran <Trittp123@gmail.com>
2022-11-03 21:46:47 -0700

    second commit

---
+++ src/AccountManager.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AccountManager {
+	ArrayList<Account> Accounts= new ArrayList<>();
+	
+	public boolean addCourse(Account newAcc){
+		if(Accounts.contains(newAcc)) {
+			return false;
+		}
+		Accounts.add(newAcc);
+		return true;
+	}
+	
+	public boolean removeCourse(Account removedAcc) {
+		if(!Accounts.contains(removedAcc)) {
+			return false;
+		}
+		Accounts.remove(removedAcc);
+		return true;
+	}
+}
