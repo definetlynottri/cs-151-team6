f94a172c38e3847059ef8920c92011893582331a
scohbears <45058690+scohbears@users.noreply.github.com>
2022-11-05 10:12:47 -0700

    Update RenameCourse.java

--- src/application/RenameCourse.java
+++ src/application/RenameCourse.java
@@ -1,15 +1,23 @@
 package application;
 
 import objects.Course;
+import java.io.IOException;
+//import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class RenameCourse {
 	@FXML
 	private TextField name;
 	@FXML
-	private Button create;
+	private Button rename;
 	
-	public void CreateCourse(ActionEvent event) throws IOexception{
+	public void CreateCourse(ActionEvent event) throws IOException {
 		String name= Main.getCurrentCourse().getName();
-		name 
+		 Main m = new Main();
+	     m.changeScene("courseBrowser.fxml");
 	}
 }
