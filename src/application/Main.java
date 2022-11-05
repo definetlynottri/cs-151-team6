4ec09420cf0a3ef745b82013f7b7e048e86d5a2b
Tri Tran <Trittp123@gmail.com>
2022-11-04 23:17:44 -0700

    added classes for course selection and creation screens with functionalities, no UI

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,12 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import objects.Account;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
     private static Stage stg;
+    private static Account currentAccount;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -24,6 +26,12 @@ public class Main extends Application {
     public void changeScene(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(getClass().getResource(fxml));
         stg.getScene().setRoot(pane);
+    }
+    public static setCurrAcc(Account acc) {
+    	this.currentAccount= acc;
+    }
+    public static Account getCurrAcc() {
+    	
     }
 
     public static void main(String[] args) {
