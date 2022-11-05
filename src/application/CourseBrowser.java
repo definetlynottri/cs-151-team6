3c7bcfeaf676c8dd05b79eb8f4201ce1bfbd7042
Tri Tran <Trittp123@gmail.com>
2022-11-05 10:48:19 -0700

    cleaned up the course browser functions and added comments

--- src/application/CourseBrowser.java
+++ src/application/CourseBrowser.java
@@ -26,17 +26,20 @@ public class CourseBrowser {
 	ComboBox<String> dropdownCourse= new ComboBox<>();
 	
 	
-	// must call it upon creating to popular dropdown
+	// must call it upon creating and refreshing to populate dropdown
 	public void Creation() {
 		ArrayList<String> options= Main.getCurrAcc().CourseListName;
 		dropdownCourse.setItems((ObservableList<String>) options);
 	}
 	
+	// goes to card screen when pressing view button with selected item, not implemented in 0.5
 	public void goToChosenCourse(ActionEvent event) throws IOException {
 		 Main m = new Main();
 	     m.changeScene("Sample.fxml");
 	     
 	}
+	
+	// goes to create screen with selected item, uses static course in main to pass to next page
 	public void goToCreateScreen(ActionEvent event) throws IOException {
 		for(int i=0; i< Main.getCurrAcc().CourseList.size(); i++) {
 			if(Main.getCurrAcc().CourseList.get(i).Name== dropdownCourse.getValue()) {
@@ -46,6 +49,8 @@ public class CourseBrowser {
 		 Main m = new Main();
 	     m.changeScene("CreateCourse.fxml");
 	}
+	
+	//goes to rename screen with selected item, uses static course in main to pass to next page
 	public void goToRenameScreen (ActionEvent event) throws IOException {
 		for(int i=0; i< Main.getCurrAcc().CourseList.size(); i++) {
 			if(Main.getCurrAcc().CourseList.get(i).Name== dropdownCourse.getValue()) {
@@ -55,9 +60,18 @@ public class CourseBrowser {
 		Main m = new Main();
 	    m.changeScene("RenameCourse.fxml");
 	}
+	
+	// deletes the item then refreshes 
 	public void RefreshAfterDeletion(ActionEvent event) throws IOException {
-		 Main m = new Main();
-	     m.changeScene("CourseBrowser.fxml");
+		for(int i=0; i< Main.getCurrAcc().CourseList.size(); i++) {
+			if(Main.getCurrAcc().CourseList.get(i).Name== dropdownCourse.getValue()) {
+				Main.getCurrAcc().CourseListName.remove(Main.getCurrAcc().CourseList.get(i).Name);
+				Main.getCurrAcc().CourseList.remove(Main.getCurrAcc().CourseList.get(i));
+			}
+		}
+		Creation();
+		Main m = new Main();
+	    m.changeScene("CourseBrowser.fxml");
 	}
 	//add logout
 	public void logout(ActionEvent event) throws IOException {
