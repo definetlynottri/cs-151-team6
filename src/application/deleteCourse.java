9b348347853c1ea3365b12fac88ed5b939ba9c04
scohbears <45058690+scohbears@users.noreply.github.com>
2022-11-05 18:19:23 -0700

    Update deleteCourse.java

--- src/application/deleteCourse.java
+++ src/application/deleteCourse.java
@@ -2,22 +2,21 @@ package application;
 
 import objects.Course;
 import java.io.IOException;
-//import java.util.ArrayList;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+//delete course
 public class deleteCourse {
 	@FXML
 	private TextField name;
 	@FXML
 	private Button delete;
 	
+	//delete course user entered and automatically go back to mainpage
 	public void delCourse(ActionEvent event) throws IOException {
-		//String name= Main.getCurrentCourse().getName();
-		
 		Main.getCurrAcc().deleteCourse(name.getText());
 		System.out.println(Main.getCurrAcc());
 		
