6d28077592f60a9668d46700b4db59a3ca56579b
scohbears <45058690+scohbears@users.noreply.github.com>
2022-11-05 17:13:18 -0700

    Update success.java

--- src/application/success.java
+++ src/application/success.java
@@ -6,11 +6,12 @@ import javafx.scene.control.Button;
 
 import java.io.IOException;
 
+//show success message and button to go back to login page
 public class success {
 
     @FXML
     private Button loginPage;
-
+    //go to loginpage 
     public void gotoLogin(ActionEvent event) throws IOException {
         Main m = new Main();
         m.changeScene("sample.fxml");
