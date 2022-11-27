6d49440a836e20ee7042bacbf2b9c9dced6309a9
Roman Shpilberg <roman.shpilberg@sjsu.edu>
2022-11-26 16:26:55 -0800

    Edit card now shows the previous card content

--- src/application/EditCard.java
+++ src/application/EditCard.java
@@ -19,6 +19,12 @@ public class EditCard {
 	
 	Card CurrentCard= Main.currentCard;
 	
+	@FXML
+	public void initialize() {
+		// Shows the current text of the card
+		UT.setText(CurrentCard.getUpperText());
+		LT.setText(CurrentCard.getLowerText());
+	}
 	
 	public void FinishedCard() throws IOException{
 		CurrentCard.UpdateCard(UT.getText(), LT.getText());
